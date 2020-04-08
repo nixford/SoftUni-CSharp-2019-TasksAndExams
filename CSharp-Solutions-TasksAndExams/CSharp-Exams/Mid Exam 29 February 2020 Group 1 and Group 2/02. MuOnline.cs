599c16b8a3a9998770b0eb8712128fd7b3cc69f5
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2020-04-08 14:33:23 +0300

    Add files via upload

---
+++ CSharp-Solutions-TasksAndExams/CSharp-Exams/Mid Exam 29 February 2020 Group 1 and Group 2/02. MuOnline.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.MuOnline
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] rooms = Console.ReadLine()
+                .Split(new string[] { "|", " " }, 
+                StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            int health = 100;
+            int bitcoins = 0;
+            int roomCount = 0;
+            bool madeIt = true;
+
+            for (int i = 0; i < rooms.Length; i+= 2)
+            {
+                roomCount++;
+                string monsterOrHealthOrBit = rooms[i];
+                int amount = int.Parse(rooms[i + 1]);
+
+                if (monsterOrHealthOrBit == "potion")
+                {
+                    if (amount + health <= 100)
+                    {
+                        Console.WriteLine($"You healed for {amount} hp.");
+                        health += amount;
+                        Console.WriteLine($"Current health: {health} hp.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"You healed for {100 - health} hp.");
+                        health = 100;
+                        Console.WriteLine($"Current health: {health} hp.");
+                    }
+                }
+                else if (monsterOrHealthOrBit == "chest")
+                {
+                    bitcoins += amount;
+                    Console.WriteLine($"You found {amount} bitcoins.");
+                }
+                else
+                {
+                    health -= amount;
+                    if (health > 0)
+                    {
+                        Console.WriteLine($"You slayed {monsterOrHealthOrBit}.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"You died! Killed by {monsterOrHealthOrBit}.");
+                        madeIt = false;
+                        break;
+                    }
+                }               
+            }
+            if (madeIt)
+            {
+                Console.WriteLine("You've made it!");
+                Console.WriteLine($"Bitcoins: {bitcoins}");
+                Console.WriteLine($"Health: {health}");
+            }
+            else
+            {
+                Console.WriteLine($"Best room: {roomCount}");
+            }
+        }
+    }
+}
---
+++ CSharp-Solutions-TasksAndExams/CSharp-Exams/Mid Exam 29 February 2020 Group 1 and Group 2/Inventory.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            List<string> itemList = Console.ReadLine()
+                    .Split(new string[] { ", " },
+                    StringSplitOptions.RemoveEmptyEntries)
+                    .ToList();
+            string command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "Craft!")
+            {
+                string[] commandLine = command
+                    .Split(new string[] { " - " }, 
+                    StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                if (commandLine[0] == "Collect")
+                {
+                    string item = commandLine[1];
+                    if (!itemList.Contains(item))
+                    {
+                        itemList.Add(item);
+                    }
+                }
+                else if (commandLine[0] == "Drop")
+                {
+                    string item = commandLine[1];
+                    if (itemList.Contains(item))
+                    {
+                        itemList.Remove(item);
+                    }
+                }
+                else if (commandLine[0] == "Combine Items")
+                {
+                    string[] items = commandLine[1].Split(':').ToArray();
+                    string oldItem = items[0];
+                    string newItem = items[1];
+                    if (itemList.Contains(oldItem))
+                    {
+                        int index = itemList.IndexOf(oldItem);
+                        itemList.Insert(index + 1, newItem);
+                    }
+                }
+                else if (commandLine[0] == "Renew")
+                {
+                    string item = commandLine[1];
+                    if (itemList.Contains(item))
+                    {
+                        itemList.Remove(item);
+                        itemList.Add(item);
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(", ", itemList));
+        }
+    }
+}
