1d62bfdc363472c4302e6697f26865614fd6f2d4
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2020-05-03 19:14:44 +0300

    Adding Solutions

---
+++ CSharp-Advanced-Tasks/Defining-classes-Lab/Car/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+        public string Make 
+        {
+            get { return this.make; }
+            set { this.make = value; }
+        }
+        public string Model { get; set; }        
+        public int Year { get; set; }
+    }
+}
