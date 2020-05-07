e6bcad020789ad7699b11bb9e1551c90f4edb771
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2020-05-07 16:02:20 +0300

    Adding solution

---
+++ Programming-Basics-Book/Drawing on the Console/Rhombus of Stars.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Square_Frame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            for (int row = 1; row <= n; row++)
+            {
+                for (int col = 1; col <= n - row; col++)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write("*");
+                for (int j = 1; j <= row - 1; j++)
+                {
+                    Console.Write(" *");
+                }
+                Console.WriteLine();
+            }
+            for (int row = 2; row <= n; row++)
+            {
+                for (int col = 1; col <= row - 1; col++)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write("*");
+                for (int j = 1; j <= n - row; j++)
+                {
+                    Console.Write(" *");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
