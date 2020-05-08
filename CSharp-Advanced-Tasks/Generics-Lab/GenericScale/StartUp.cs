ea697d56e761f9de98d34932878282a5bfcff2d3
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2020-05-08 17:38:32 +0300

    Adding solutions

---
+++ CSharp-Advanced-Tasks/Generics-Lab/GenericScale/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GenericScale
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var scale = new EqualityScale<int>(5, 5);
+
+            Console.WriteLine(scale.AreEqual(5,5));
+        }
+    }
+}
