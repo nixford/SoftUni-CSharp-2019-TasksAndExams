a715583f524924b3a4090617f0fd90028f19055a
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2020-05-08 14:16:07 +0300

    Adding solution

---
+++ Programming-Basics-Book/Drawing on the Console/Square Frame.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Square_Frame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            Console.Write("+ ");
+            for (int i = 1; i <= n - 2; i++)
+            {
+
+                Console.Write("-" + " ");
+            }
+            Console.WriteLine("+");
+
+
+            for (int i = 1; i <= n - 2; i++)
+            {
+                Console.Write("| ");
+                for (var j = 1; j <= n - 2; j++)
+                {
+                    Console.Write("-" + " ");
+                }
+                Console.WriteLine("|");
+            }
+
+            Console.Write("+ ");
+            for (int i = 1; i <= n - 2; i++)
+            {
+
+                Console.Write("-" + " ");
+            }
+            Console.WriteLine("+");
+        }
+    }
+}
