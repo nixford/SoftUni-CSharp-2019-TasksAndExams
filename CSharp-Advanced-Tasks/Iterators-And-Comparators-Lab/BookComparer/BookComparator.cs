5a2505860fd8e8d39fd7ce2e7808b033205319b0
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2020-05-13 18:11:42 +0300

    Adding solutions - Iterators and Comparators

---
+++ CSharp-Advanced-Tasks/Iterators-And-Comparators-Lab/BookComparer/BookComparator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class BookComparator : IComparer<Book>
+    {
+        public int Compare(Book bookOne, Book bookTwo)
+        {
+            int result = bookOne.Title.CompareTo(bookTwo.Title);
+            if (result == 0)
+            {
+                result = bookTwo.Year.CompareTo(bookOne.Year);
+            }
+            return result;
+        }
+    }
+}
