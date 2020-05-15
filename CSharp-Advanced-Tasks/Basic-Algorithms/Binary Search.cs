b9415940fa79482b5c08483d4f7a26e50f6d2d92
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2020-05-15 15:43:49 +0300

    Adding solutions - Basic Algorithms

---
+++ CSharp-Advanced-Tasks/Basic-Algorithms/Binary Search.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+
+namespace Binary_Search
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int key = int.Parse(Console.ReadLine());
+
+            if (input == String.Empty)
+            {
+                return;
+            }
+
+            int[] arr = input
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int index = BinarySearch.IndexOf(arr, key);
+            Console.WriteLine(index);
+        }
+
+        public class BinarySearch
+        {
+            public static int IndexOf(int[] arr, int key)
+            {
+                int lo = 0;
+                int hi = arr.Length - 1;
+
+                while (lo <= hi)
+                {
+                    int mid = lo + (hi - lo) / 2;
+
+                    if (key < arr[mid])
+                    {
+                        hi = mid - 1;
+                    }
+                    else if (key > arr[mid])
+                    {
+                        lo = mid + 1;
+                    }
+                    else
+                    {
+                        return mid;
+                    }
+                }
+
+                return -1;
+            }
+        }
+    }
+}
