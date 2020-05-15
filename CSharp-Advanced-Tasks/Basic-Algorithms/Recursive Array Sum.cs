f9aa1f83bf51d32a5fcb1e360c8a03d83e2ebf78
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2020-05-15 13:01:19 +0300

    Adding solution - Basic-Algorithms

---
+++ CSharp-Advanced-Tasks/Basic-Algorithms/Recursive Array Sum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace Recursive_Array_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();            
+
+            Console.WriteLine(Sum(input));
+        }
+        public static int Sum(int[] arr, int index = 0)
+        {
+            if (index == arr.Length)
+            {
+                return 0;
+            }
+            return arr[index] + Sum(arr, index + 1);
+        }
+    }
+}
---
+++ CSharp-Advanced-Tasks/Basic-Algorithms/Recursive Factorial.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Recursive_Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Factorial(n));
+        }
+        public static long Factorial(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+            return n * Factorial(n - 1);            
+        }
+    }
+}
