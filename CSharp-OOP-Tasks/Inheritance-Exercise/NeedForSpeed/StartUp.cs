31c8b3782d8bbe04e74d5163e1663abcd9db355e
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2020-05-28 20:35:14 +0300

    Adding solutions

---
+++ CSharp-OOP-Tasks/Inheritance-Exercise/NeedForSpeed/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace NeedForSpeed
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Vehicle vehicle = new Vehicle(300, 100);
+            Vehicle car = new Car(300, 100);
+
+            vehicle.Drive(10);
+            car.Drive(10);
+
+            System.Console.WriteLine(vehicle.Fuel);
+            System.Console.WriteLine(car.Fuel);
+        }
+    }
+}
