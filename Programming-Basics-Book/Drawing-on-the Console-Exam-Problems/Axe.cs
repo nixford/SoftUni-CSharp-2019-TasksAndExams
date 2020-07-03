25737f200b289f33feec3f375865ca83cd81f01c
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2020-07-03 16:13:23 +0300

    Adding Solutions

---
+++ Programming-Basics-Book/Drawing-on-the Console-Exam-Problems/Axe.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Axe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("{0}*{1}*{2}", new string('-', 3 * n), new string('-', i), new string('-', 2 * n - 2 - i));
+            }
+            for (int i = 0; i < n / 2; i++)
+            {
+                Console.WriteLine("{0}*{1}*{1}", new string('*', n * 3), new string('-', n - 1));
+            }
+            for (int i = 0; i < n / 2; i++)
+            {
+                if (i == n / 2 - 1)
+                {
+                    Console.WriteLine("{0}*{1}*{2}", new string('-', (3 * n) - i), new string('*', n - 1 + (i * 2)), new string('-', n - 1 - i));
+                }
+                else
+                {
+                    Console.WriteLine("{0}*{1}*{2}", new string('-', n * 3 - i), new string('-', n - 1 + i + i), new string('-', n - 1 - i));
+                }
+            }
+        }
+    }
+}
---
+++ Programming-Basics-Book/Drawing-on-the Console-Exam-Problems/Stop.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int dots = n + 1;
+            int underscopes = 2 * n + 1;
+
+            // FIRST ROW
+            Console.WriteLine("{0}{1}{0}", new string('.', dots), new string('_', underscopes));
+
+            // TOP MIDDLE
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("{0}//{1}\\\\{0}", new string('.', dots - 1), new string('_', underscopes - 2));
+                underscopes += 2;
+                dots -= 1;
+            }
+
+            // MIDDLE ROW 
+            Console.WriteLine("//{0}STOP!{0}\\\\", new string('_', (underscopes - 6) / 2));
+
+            // BOTTOM ROWS
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("{0}\\\\{1}//{0}", new string('.', i), new string('_', underscopes - 2));
+
+                underscopes -= 2;
+            }
+        }
+    }
+}
