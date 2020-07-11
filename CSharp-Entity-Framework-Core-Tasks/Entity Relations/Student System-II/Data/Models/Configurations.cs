89ec0e28683b179e85c5e93afcdbdf6daa248bd6
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2020-07-12 00:58:38 +0300

    Adding solution - Student System-II

---
+++ CSharp-Entity-Framework-Core-Tasks/Entity Relations/Student System-II/Data/Models/Configurations.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03_FootballBetting.Data.Models
+{
+    internal class Configurations
+    {
+        public const string ConnectionString =
+            "Server=.\\SQLEXPRESS;Database=FootballBetting;Integrated Security=true";
+    }
+}
---
+++ CSharp-Entity-Framework-Core-Tasks/Entity Relations/Student System-II/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using P03_FootballBetting.Data;
+using System;
+
+namespace P03_FootballBetting
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            FootballBettingContext fb = new FootballBettingContext();
+
+            fb.Database.EnsureCreated();
+        }
+    }
+}
