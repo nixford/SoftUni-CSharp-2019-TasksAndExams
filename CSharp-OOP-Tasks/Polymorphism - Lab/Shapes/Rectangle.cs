8b28798af98348a7f825d2e884b72ab440b49d60
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2020-07-16 15:49:23 +0300

    Adding solutions - Polymorphism-Lab

---
+++ CSharp-OOP-Tasks/Polymorphism - Lab/Shapes/Rectangle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        private readonly double height;
+
+        private readonly double width;
+
+        public Rectangle(double height, double width)
+        {
+            this.height = height;
+            this.width = width;
+
+        }
+
+        public override double CalculateArea()
+        {
+            return width * height;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return (2 * width) + (2 * height);
+        }
+
+        public override string Draw()
+        {
+            return base.Draw() + "Rectangle";
+        }        
+    }
+}
