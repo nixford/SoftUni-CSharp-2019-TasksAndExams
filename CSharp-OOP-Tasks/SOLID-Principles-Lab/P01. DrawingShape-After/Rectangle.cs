448ee06445aab63ef07788d7b96eb97e35fd7531
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2020-07-19 20:51:05 +0300

    Adding solution - SOLID-Principles-Lab

---
+++ CSharp-OOP-Tasks/SOLID-Principles-Lab/P01. DrawingShape-After/Rectangle.cs
@@ -0,0 +1,25 @@
+﻿using P01._DrawingShape_After.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01._DrawingShape_After
+{
+    public class Rectangle : IShape
+    {
+        public Rectangle(double width, double height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public double Width { get; set; }
+
+        public double Height { get; set; }
+
+        public double Area
+        {
+            get { return this.Width * this.Height; }
+        }        
+    }
+}
