c7fcf7d9914469c1b9b433c636215db5c7694293
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2020-07-20 13:58:28 +0300

    Adding solutions - SOLID-Principles-Lab

---
+++ CSharp-OOP-Tasks/SOLID-Principles-Lab/01. SRP/P02. Books - After/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P02._Books___After
+{
+    public class Book
+    {
+        public string Title { get; set; }
+
+        public string Author { get; set; }        
+
+        public string TurnPage(int page)
+        {
+            return "Current page";
+        }
+    }
+}
---
+++ CSharp-OOP-Tasks/SOLID-Principles-Lab/01. SRP/P02. Books - After/Library.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P02._Books___After
+{
+    public class Library
+    {
+        private List<Book> books;
+
+        public Library()
+        {
+            this.books = new List<Book>();
+        }
+
+        public int FindBook(string title)
+        {
+            //Logic
+            return 42;
+        }
+    }
+}
