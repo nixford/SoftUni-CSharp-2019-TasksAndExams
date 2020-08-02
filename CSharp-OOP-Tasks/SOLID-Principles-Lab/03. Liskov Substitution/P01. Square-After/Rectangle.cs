490b28d3e181f9485f5402d3d7220eae311b7fbd
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2020-08-02 17:58:49 +0300

    Adding solutions -  Liskov Substitution

---
+++ CSharp-OOP-Tasks/SOLID-Principles-Lab/03. Liskov Substitution/P01. Square-After/Rectangle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01._Square_After
+{
+    public class Rectangle : Shape
+    {
+        public virtual double Width { get; set; }
+
+        public virtual double Height { get; set; }
+
+        public override double Area => this.Width * this.Height;
+    }
+}
---
+++ CSharp-OOP-Tasks/SOLID-Principles-Lab/03. Liskov Substitution/P01. Square-After/Square.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01._Square_After
+{
+    public class Square : Shape
+    {
+        public double Side { get; set; }
+
+        public override double Area => this.Side * this.Side;
+    }
+}
