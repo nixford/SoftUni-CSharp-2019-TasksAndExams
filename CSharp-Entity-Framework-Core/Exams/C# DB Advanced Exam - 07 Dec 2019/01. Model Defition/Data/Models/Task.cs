fc1a1bf060477b637f17751f54ec8bc5177410ed
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2020-08-05 21:01:17 +0300

    Adding solutions - C# DB Advanced Exam - 07 Dec 2019

---
+++ CSharp-Entity-Framework-Core/Exams/C# DB Advanced Exam - 07 Dec 2019/01. Model Defition/Data/Models/Task.cs
@@ -0,0 +1,40 @@
+﻿using Castle.Components.DictionaryAdapter;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using TeisterMask.Data.Models.Enums;
+using KeyAttribute = System.ComponentModel.DataAnnotations.KeyAttribute;
+
+namespace TeisterMask.Data.Models
+{
+    public class Task
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(2)]
+        [MaxLength(40)]
+        public string Name { get; set; }
+
+        [Required]
+        public DateTime OpenDate { get; set; }
+
+        [Required]
+        public DateTime DueDate { get; set; }
+
+        [Required]
+        public ExecutionType ExecutionType { get; set; }
+
+        [Required]
+        public LabelType LabelType { get; set; }
+
+        [Required]        
+        public int ProjectId { get; set; }
+        public Project Project { get; set; }
+
+        public ICollection<EmployeeTask> EmployeesTasks { get; set; }
+    }
+}
