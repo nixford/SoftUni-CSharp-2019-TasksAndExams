988d37699633224f22d4eab851f0f00b4ec88017
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2020-08-06 18:35:26 +0300

    Adding solutions - C# DB Advanced Exam - 07 Dec 2019

---
+++ CSharp-Entity-Framework-Core/Exams/C# DB Advanced Exam - 07 Dec 2019/02. Data Import/Data/TeisterMaskContext.cs
@@ -0,0 +1,70 @@
+﻿namespace TeisterMask.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using TeisterMask.Data.Models;
+
+    public class TeisterMaskContext : DbContext
+    {
+        public TeisterMaskContext() 
+        {
+            
+        }
+
+        public TeisterMaskContext(DbContextOptions options)
+            : base(options) 
+        { 
+        
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<EmployeeTask> EmployeesTasks { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Task> Tasks { get; set; }       
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseSqlServer(Configuration.ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder
+                .Entity<Project>(entity =>
+                {
+                    entity
+                        .HasKey(p => p.Id);
+
+                    entity
+                        .HasMany(p => p.Tasks)
+                        .WithOne(p => p.Project)
+                        .HasForeignKey(t => t.ProjectId);
+
+                });
+
+
+            modelBuilder
+                .Entity<EmployeeTask>(entity => 
+                {
+                    entity
+                        .HasKey(et => new { et.EmployeeId, et.TaskId });
+
+                    entity
+                        .HasOne(et => et.Employee)
+                        .WithMany(et => et.EmployeesTasks)
+                        .HasForeignKey(et => et.EmployeeId);
+
+                    entity
+                        .HasOne(et => et.Task)
+                        .WithMany(et => et.EmployeesTasks)
+                        .HasForeignKey(et => et.TaskId);
+
+                });
+        }
+    }
+}
---
+++ CSharp-Entity-Framework-Core/Exams/C# DB Advanced Exam - 07 Dec 2019/02. Data Import/DataProcessor/ImportDto/ImportProjectTasksDTO.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Xml.Serialization;
+using TeisterMask.Data.Models.Enums;
+
+namespace TeisterMask.DataProcessor.ImportDto
+{
+    [XmlType("Task")]
+    public class ImportProjectTasksDTO
+    {
+       
+        [Required]
+        [MinLength(2)]
+        [MaxLength(40)]
+        [XmlElement("Name")]
+        public string Name { get; set; }
+
+       
+        [Required]
+        [XmlElement("OpenDate")]
+        public string OpenDate { get; set; } // string instead DateTime
+
+        
+        [Required]
+        [XmlElement("DueDate")]
+        public string DueDate { get; set; } // string instead DateTime
+        
+        [Required]
+        [Range(0, 3)]
+        [XmlElement("ExecutionType")]
+        public int ExecutionType { get; set; } // int instead enum in order to be accepted with cast in code logic
+                
+        [Required]
+        [Range(0, 4)]
+        [XmlElement("LabelType")]
+        public int LabelType { get; set; }
+    }
+}
