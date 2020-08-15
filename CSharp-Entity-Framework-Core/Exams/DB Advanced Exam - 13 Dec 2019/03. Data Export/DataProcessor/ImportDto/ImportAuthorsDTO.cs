cdc305d79ce83af6208f703400b95abbdff3c341
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2020-08-15 10:40:52 +0300

    Adding solutions - DB Advanced Exam - 13 Dec 2019

---
+++ CSharp-Entity-Framework-Core/Exams/DB Advanced Exam - 13 Dec 2019/03. Data Export/DataProcessor/ImportDto/ImportAuthorsDTO.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BookShop.DataProcessor.ImportDto
+{
+    public class ImportAuthorsDTO
+    {
+        [Required]
+        [MinLength(3)]
+        [MaxLength(30)]
+        [JsonProperty("FirstName")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(30)]
+        [JsonProperty("LastName")]
+        public string LastName { get; set; }        
+
+        [Required]
+        [RegularExpression("^[0-9]{3}-[0-9]{3}-[0-9]{4}$")]
+        [JsonProperty("Phone")]
+        public string Phone { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [JsonProperty("Email")]
+        public string Email { get; set; }
+
+        public AuthorsBooksArrayDTO[] Books { get; set; }
+    }
+}
---
+++ CSharp-Entity-Framework-Core/Exams/DB Advanced Exam - 13 Dec 2019/03. Data Export/DataProcessor/Serializer.cs
@@ -0,0 +1,74 @@
+﻿namespace BookShop.DataProcessor
+{
+    using System;
+    using System.Globalization;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Xml;
+    using System.Xml.Serialization;
+    using BookShop.Data.Models;
+    using BookShop.Data.Models.Enums;
+    using BookShop.DataProcessor.ExportDto;
+    using Data;
+    using Newtonsoft.Json;
+    using Formatting = Newtonsoft.Json.Formatting;
+
+    public class Serializer
+    {
+        public static string ExportMostCraziestAuthors(BookShopContext context)
+        {
+            var authors = context
+                .Authors
+                .Select(a => new
+                {
+                    AuthorName = $"{a.FirstName} {a.LastName}",
+                    Books = a.AuthorsBooks
+                            .OrderByDescending(b => b.Book.Price)
+                            .Select(b => new
+                            {
+                                BookName = b.Book.Name,
+                                BookPrice = b.Book.Price.ToString("F2"),
+                            })
+                            .ToArray()
+                })
+                .ToArray()
+                .OrderByDescending(a => a.Books.Length)
+                .ThenBy(a => a.AuthorName)
+                .ToArray();
+
+
+
+            var jsonString = JsonConvert.SerializeObject(authors, Formatting.Indented);
+
+            return jsonString;
+        }
+
+        public static string ExportOldestBooks(BookShopContext context, DateTime date)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var books = context
+                .Books
+                .Where(b => b.PublishedOn < date && b.Genre == Genre.Science)
+                .ToArray()
+                .OrderByDescending(p => p.Pages)
+                .ThenByDescending(b => b.PublishedOn)
+                .Take(10)
+                .Select(b => new ExportBookDTO
+                {
+                    Name = b.Name,
+                    Pages = b.Pages,
+                    PublishedOn = b.PublishedOn.ToString("d", CultureInfo.InvariantCulture),
+                })
+                .ToArray();
+
+
+            var xmlSerializer = new XmlSerializer(typeof(ExportBookDTO[]), new XmlRootAttribute("Books"));
+            var namespaces = new XmlSerializerNamespaces(new[] { XmlQualifiedName.Empty });
+            xmlSerializer.Serialize(new StringWriter(sb), books, namespaces);
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
