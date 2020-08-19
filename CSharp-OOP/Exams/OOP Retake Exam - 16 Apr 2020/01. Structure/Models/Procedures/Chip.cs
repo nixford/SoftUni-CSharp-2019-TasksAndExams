26ce06f511bbaae4e574275a084a51309e81c18d
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2020-08-19 20:12:31 +0300

    Adding solutions - OOP Retake Exam - 16 Apr 2020

---
+++ CSharp-OOP/Exams/OOP Retake Exam - 16 Apr 2020/01. Structure/Models/Procedures/Chip.cs
@@ -0,0 +1,24 @@
+﻿using RobotService.Models.Robots.Contracts;
+using RobotService.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RobotService.Models.Procedures
+{
+    public class Chip : Procedure
+    {
+        public override void DoService(IRobot robot, int procedureTime)
+        {
+            base.DoService(robot, procedureTime);
+            if (robot.IsChipped == true)
+            {
+                throw new ArgumentException(String.Format(ExceptionMessages.AlreadyChipped, robot.Name));
+            }
+
+            robot.IsChipped = true;
+            robot.Happiness -= 5;
+            Robots.Add(robot);
+        }        
+    }
+}
