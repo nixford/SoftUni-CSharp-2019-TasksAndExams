ffe5236def2405bfce9880e79aba07f8e9308d2b
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2020-08-29 10:19:37 +0300

    Adding solutions - OOP Retake Exam - 19 Dec 2019

---
+++ CSharp-OOP/Exams/OOP Retake Exam - 19 Dec 2019/03. Unit Tests/PresentsTests.cs
@@ -0,0 +1,132 @@
+﻿namespace Presents.Tests
+{
+    using NUnit.Framework;
+    using System;
+
+    [TestFixture]
+    public class PresentsTests
+    {
+        private Bag bag;
+
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Constructor_ShouldBeNullValues()
+        {
+            bag = new Bag();
+            Assert.AreEqual(bag.GetPresents().Count, 0);
+        }
+
+        [Test]
+        public void Create_PresentIsNullException()
+        {
+            bag = new Bag();
+            Assert.Throws<ArgumentNullException>(() =>
+                   bag.Create(null));
+        }
+
+        [Test]
+        public void Create_AlreadyExistException()
+        {
+            bag = new Bag();
+            Present present1 = new Present("Truck", 10);
+            Present present2 = new Present("Ball", 15);
+            bag.Create(present1);
+            bag.Create(present2);
+
+            Assert.Throws<InvalidOperationException>(() =>
+                   bag.Create(present1));
+        }
+
+        [Test]
+        public void Create_CorrectlyNewPresent()
+        {
+            bag = new Bag();
+
+            Present present1 = new Present("Truck", 10);
+            
+            bag.Create(present1);
+            
+            Assert.AreEqual(bag.GetPresents().Count, 1);
+        }
+
+        [Test]
+        public void Create_ReturnCorrectlyString()
+        {
+            bag = new Bag();
+
+            Present present1 = new Present("Truck", 10);
+            
+            string actualResult = bag.Create(present1);
+            string expectedResult = $"Successfully added present {present1.Name}.";
+
+            Assert.AreEqual(actualResult, expectedResult);
+        }
+
+        [Test]
+        public void Remove_ReturnCorrectly()
+        {
+            bag = new Bag();
+
+            Present present1 = new Present("Truck", 10);
+            bag.Create(present1);
+            
+
+            Assert.AreEqual(bag.Remove(present1), true);
+        }
+
+        [Test]
+        public void GetPresentWithLeastMagic_CorrectResult()
+        {
+            bag = new Bag();
+
+            Present present1 = new Present("Truck", 10);
+            Present present2 = new Present("Ball", 15);
+            bag.Create(present1);
+            bag.Create(present2);
+
+            Present actualResult = bag.GetPresentWithLeastMagic();
+            Present expectedResult = present1;
+
+            Assert.AreEqual(actualResult.Magic, expectedResult.Magic);
+        }
+
+        [Test]
+        public void GetPresent_ShouldReturnNull()
+        {
+            bag = new Bag();
+
+            Assert.AreEqual(bag.GetPresent("Truck"), null);
+        }
+
+        [Test]
+        public void GetPresent_ShouldReturnValue()
+        {
+            bag = new Bag();
+
+            Present present1 = new Present("Truck", 10);
+            Present present2 = new Present("Ball", 15);
+            bag.Create(present1);
+            bag.Create(present2);
+
+            Assert.AreEqual(bag.GetPresent("Truck"), present1);
+        }
+
+        [Test]
+        public void GetPresents_ShouldReturnCount()
+        {
+            bag = new Bag();
+
+            Present present1 = new Present("Truck", 10);
+            Present present2 = new Present("Ball", 15);
+            bag.Create(present1);
+            bag.Create(present2);
+
+            Assert.AreEqual(bag.GetPresents().Count, 2);
+        }
+    }
+}
