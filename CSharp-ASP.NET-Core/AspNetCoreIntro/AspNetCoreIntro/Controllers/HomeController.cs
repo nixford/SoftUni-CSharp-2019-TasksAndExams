7ee2d7fb8be882d7e06838a3c9f346eb95d28aac
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2020-09-16 12:34:53 +0300

    Adding AspNetCoreIntro - Lab

---
+++ CSharp-ASP.NET-Core/AspNetCoreIntro/AspNetCoreIntro/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using AspNetCoreIntro.Models;
+using AspNetCoreIntro.Data;
+
+namespace AspNetCoreIntro.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext applicationDbContext;
+
+        public HomeController(ILogger<HomeController> logger, 
+            ApplicationDbContext applicationDbContext)
+        {
+            _logger = logger;
+            this.applicationDbContext = applicationDbContext;
+        }
+
+        public IActionResult Index()
+        {
+            return this.RedirectToAction("Add", "Demos");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
