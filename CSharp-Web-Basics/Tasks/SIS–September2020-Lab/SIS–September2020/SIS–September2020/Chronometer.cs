7d63e40176b4159e8cfbbd480da5350b9cf0d6d4
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2020-09-20 14:30:22 +0300

    commit Chronometer

---
+++ CSharp-Web-Basics/Tasks/SIS–September2020-Lab/SIS–September2020/SIS–September2020/Chronometer.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace SIS_September2020
+{
+    public class Chronometer : IChronometer
+    {
+        private Stopwatch stopwatch;
+        private TimeSpan timeSpan;
+        private ICollection<string> lapsList;
+
+        public Chronometer()
+        {
+            this.lapsList = new HashSet<string>();
+            this.stopwatch = new Stopwatch();
+            this.timeSpan = new TimeSpan();
+        }
+
+        public void Start()
+        {
+            stopwatch.Start();
+        }
+
+        public void Stop()
+        {            
+            timeSpan = stopwatch.Elapsed;
+        }
+
+        public string Lap()
+        {
+            TimeSpan ts = stopwatch.Elapsed;            
+            var result = string.Format($"" +
+                $"{ts.Minutes:D2}" +
+                $":{ts.Seconds:D2}" +
+                $":{ts.Milliseconds:D4}");
+            lapsList.Add(result);
+            return result;
+        }
+
+        public List<string> Laps => lapsList.ToList();
+
+        public string GetTime 
+            => string.Format("{0:D2}:{1:D2}:{2:D4}", 
+                timeSpan.Minutes, 
+                timeSpan.Seconds, 
+                timeSpan.Milliseconds);            
+
+        public void Reset()
+        {            
+            stopwatch.Reset();
+            lapsList.Clear();
+        }        
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            int count = 0;
+
+            sb.AppendLine("Laps:");
+            foreach (var lap in lapsList)
+            {                
+                sb.AppendLine($"{count}. {lap}");
+                count++;
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ CSharp-Web-Basics/Tasks/SIS–September2020-Lab/SIS–September2020/SIS–September2020/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace SIS_September2020
+{
+    public class Program
+    {        
+        static void Main(string[] args)
+        {      
+            string inputCommand = string.Empty;
+            Chronometer chronometer = new Chronometer();
+
+            while ((inputCommand = Console.ReadLine()) != "exit")            
+            {
+                if (inputCommand == "start")
+                {
+                    chronometer.Start();
+                }
+                else if (inputCommand == "stop")
+                {
+                    chronometer.Stop();
+                }
+                else if (inputCommand == "lap")
+                {
+                    Console.WriteLine(chronometer.Lap());
+                }
+                else if (inputCommand == "laps")
+                {
+                    Console.WriteLine(chronometer.ToString());
+                }
+                else if (inputCommand == "time")
+                {
+                    Console.WriteLine(chronometer.GetTime);
+                }
+                else if (inputCommand == "reset")
+                {
+                    chronometer.Reset();
+                }                
+            }
+        }
+    }
+}
