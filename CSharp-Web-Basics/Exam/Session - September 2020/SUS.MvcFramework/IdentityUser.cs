949f295aef9a26fb612360db6f5b350c263424cf
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2020-10-25 14:22:29 +0200

    Add SUS platform code

---
+++ CSharp-Web-Basics/Exam/Session - September 2020/SUS.MvcFramework/IdentityUser.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SUS.MvcFramework
+{
+    public class IdentityUser<T>
+    {
+        public T Id { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Username { get; set; }
+
+        [Required]
+        // [MaxLength(20)] After hashing of the password, it becomes longer than 20 symbols:
+        // SqlException: String or binary data would be truncated in table 'IRunes.dbo.Users', column 'Password'. Truncated value: '28618b267b8397a72ead'.
+        // The statement has been terminated.
+        public string Password { get; set; }
+
+        [Required]
+        public string Email { get; set; }        
+
+        public IdentityRole Role { get; set; }
+    }
+}
